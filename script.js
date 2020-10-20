128087a29e45ddc57f0db50c86f77d21b0015b72
Overchukisjedi <Overchuk300@gmail.com>
2020-10-20 17:24:06 +0300

    commit on "gh-pages"

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 let score = 0;
+
+const brickRowCount = 9;
+const brickColumnCount = 5;
+const delay = 500;
+
+const ball = {
+    x: canvas.width / 2,
+    y: canvas.height / 2,
+    size: 10,
+    speed: 4,
+    dx: 4,
+    dy: -4,
+    visible: true
+};
